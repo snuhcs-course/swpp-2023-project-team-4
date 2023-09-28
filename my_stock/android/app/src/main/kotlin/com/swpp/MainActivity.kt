dc59fb4c5573d821e0dee6c7ea74196039f01b38
LimSusu <dlatn4389@snu.ac.kr>
2023-09-28 15:23:13 +0900

    :tada: begin flutter project

---
+++ my_stock/android/app/src/main/kotlin/com/swpp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.swpp.my_stock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ my_stock/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:my_stock/app/app.dart';
+import 'package:my_stock/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
