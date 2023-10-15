be9332fbfebd0816b2692af84bfe29f256d05db7
bosun1043 <bosun104339@gmail.com>
2023-10-15 23:13:29 +0900

    production 세팅

--- my_stock/lib/main_production.dart
+++ my_stock/lib/main_production.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:my_stock/bootstrap.dart';
 
 import 'app.dart';
+import 'firebase_options/firebase_options_dev.dart';
 
-void main() {
+void main() async {
   bootstrap(() => const App());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
