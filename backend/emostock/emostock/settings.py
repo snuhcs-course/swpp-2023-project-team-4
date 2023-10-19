c6f89c886ef0e457e899d0f0a7efc1f7c8dc5bfe
Keunyoung Kim <keunyoung98@snu.ac.kr>
2023-10-20 03:01:06 +0900

    Custom user model 생성

--- backend/emostock/emostock/settings.py
+++ backend/emostock/emostock/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+DUMMY_PW = 'dummypw12'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
