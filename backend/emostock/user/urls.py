73bcd6717ee1e2fda7911891c0692b62a3987488
Keunyoung Kim <keunyoung98@snu.ac.kr>
2023-10-20 03:30:56 +0900

    회원가입 API 구현

--- backend/emostock/user/urls.py
+++ backend/emostock/user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('account/', include('allauth.urls')),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- backend/emostock/user/views.py
+++ backend/emostock/user/views.py
@@ -1,5 +1,26 @@
 import requests
 
 from django.shortcuts import render
+from django.urls import reverse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from django.conf import settings
+DUMMY_PW = getattr(settings, 'DUMMY_PW', None)
+
+
+class SignUpView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        req_url = request.build_absolute_uri(reverse('signup').replace('signup/', 'dj-rest-auth/registration/'))
+        data = request.data
+        data.update({'username': request.data['google_id'], 'password1': DUMMY_PW, 'password2': DUMMY_PW})
+
+        r = requests.post(req_url, data=data)
+        if r.status_code == 201:
+            res_payload = {"access_token": r.json()['access'], "refresh_token": r.json()['refresh']}
+            return Response(res_payload, status=201)
+        else:
+            return Response(r.json(), status=400)
