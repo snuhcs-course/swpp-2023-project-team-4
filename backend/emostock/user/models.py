57b5bd4f5c92a223fbcb50499b1a933a892ceb90
Junseok Lee <123jellybean@snu.ac.kr>
2023-10-20 02:39:05 +0900

    Create and setup project
    * Create a Django project
    * Modify settings.py
    * Create 'user' app

---
+++ backend/emostock/user/models.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+# Create your models here.
---
+++ backend/emostock/user/views.py
@@ -0,0 +1,5 @@
+import requests
+
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
