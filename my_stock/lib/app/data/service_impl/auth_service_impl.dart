cea010a2bc82bfb03007737fb6d74423f64ce59f
bosun1043 <bosun104339@gmail.com>
2023-10-23 00:29:11 +0900

    로그인 api 연결 준비랑 pubspec.yaml 전체 업데이트

--- my_stock/lib/app/data/service_impl/auth_service_impl.dart
+++ my_stock/lib/app/data/service_impl/auth_service_impl.dart
@@ -1,25 +1,40 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:my_stock/app/data/util/http_util.dart';
 import 'package:my_stock/app/domain/result.dart';
 import 'package:my_stock/app/domain/service_interface/auth_service.dart';
 
 class AuthServiceImpl implements AuthService {
-  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
   @override
   Future<Result<void, Enum>> signInByGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAccount? googleUser;
     try {
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth?.accessToken,
-        idToken: googleAuth?.idToken,
-      );
-      await _firebaseAuth.signInWithCredential(credential);
+      googleUser = await GoogleSignIn().signIn();
     } catch (e) {
       return Fail(DefaultIssue.badRequest);
     }
-
+    String googleId = googleUser!.id;
+    print("googleId: $googleId");
     return Success(null);
+  }
+
+  @override
+  Future<Result<void, Enum>> autoSignIn() async {
+    if (HttpUtil.I.isAuthorized) {
+      return Success(null);
+    } else {
+      return Fail(DefaultIssue.unAuthorized);
+    }
+  }
+
+  @override
+  Future<Result<void, Enum>> signOut() async {
+    await HttpUtil.I.deleteAccessToken();
+    return Success(null);
+  }
+
+  @override
+  Future<Result<void, Enum>> signUpByGoogle({required String nickname}) {
+    // TODO: implement signUpByGoogle
+    throw UnimplementedError();
   }
 }
