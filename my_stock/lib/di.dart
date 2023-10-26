b42c0d28c0868ab837882d43788a99ae8a6b0785
bosun1043 <bosun104339@gmail.com>
2023-10-26 13:55:58 +0900

    chore: test commit

--- my_stock/lib/di.dart
+++ my_stock/lib/di.dart
@@ -12,5 +12,6 @@ void setup() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   getIt.registerSingleton<SharedPreferences>(prefs);
   getIt.registerSingleton<AuthService>(AuthServiceImpl());
+
   getIt.registerSingleton<LocalRepository>(LocalRepositoryImpl());
 }
