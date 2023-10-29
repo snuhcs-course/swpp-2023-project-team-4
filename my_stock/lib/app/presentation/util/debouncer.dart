9de9e9a53e3fb2eb2fa6e673b39047e03cf720d3
LimSusu <dlatn4389@snu.ac.kr>
2023-10-29 18:28:54 +0900

    :sparkles: Debouncer 추가

---
+++ my_stock/lib/app/presentation/util/debouncer.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+class Debouncer {
+  final Duration delay;
+  Timer? _timer;
+
+  Debouncer({required this.delay});
+
+  void call(void Function() action) {
+    _timer?.cancel();
+    _timer = Timer(delay, action);
+  }
+}
