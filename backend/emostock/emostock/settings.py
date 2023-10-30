c8e3272b5982057222cd3ce1ea2964c7d04d9fa6
Yeonho Lee <6464106@snu.ac.kr>
2023-10-30 23:36:52 +0900

    Changes for deployment
    * Add allowed hosts
    * Use python-decouple to get environment variables
    * Update requirements.txt

--- backend/emostock/emostock/settings.py
+++ backend/emostock/emostock/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -22,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.ap-northeast-2.compute.amazonaws.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -86,11 +91,11 @@ WSGI_APPLICATION = 'emostock.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DB_NAME = os.environ.get("DB_NAME")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = config("DB_NAME")
+DB_HOST = config("DB_HOST")
+DB_PORT = config("DB_PORT")
+DB_USER = config("DB_USER")
+DB_PASSWORD = config("DB_PASSWORD")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
