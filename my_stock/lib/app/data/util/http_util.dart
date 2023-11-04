729a1cef4b364598d3f22bdd01729ee0946c6fb0
LimSusu <dlatn4389@snu.ac.kr>
2023-11-05 06:18:28 +0900

    :sparkles: BaseUrl 설정

--- my_stock/lib/app/data/util/http_util.dart
+++ my_stock/lib/app/data/util/http_util.dart
@@ -7,7 +7,7 @@ class HttpUtil {
   SharedPreferences _pref = GetIt.I.get<SharedPreferences>();
 
   HttpUtil._() {
-    _dio.options.baseUrl = 'https://emostock-18fz.onrender.com';
+    _dio.options.baseUrl = 'http://ec2-52-78-75-7.ap-northeast-2.compute.amazonaws.com:3000';
     _dio.interceptors.add(
       InterceptorsWrapper(onRequest: (options, handler) {
         options.headers['Authorization'] = 'Bearer ${_pref.getString('access_token')}';
