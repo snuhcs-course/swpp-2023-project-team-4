66ad8ca24d5bae408e296d57c834303163f0156d
LimSusu <dlatn4389@snu.ac.kr>
2023-11-08 23:37:30 +0900

    :sparkles: Pair util

--- my_stock/lib/core/util/pair.dart
+++ my_stock/lib/core/util/pair.dart
@@ -4,3 +4,11 @@ class Pair<T1, T2> {
 
   const Pair(this.first, this.second);
 }
+
+class ThirdPair<T1, T2, T3> {
+  final T1 first;
+  final T2 second;
+  final T3 third;
+
+  const ThirdPair(this.first, this.second, this.third);
+}
