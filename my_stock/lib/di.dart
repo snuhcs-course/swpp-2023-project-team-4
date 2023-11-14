051f2babc2da451f58c8b2d1132b81f5de467893
LimSuu <dlatn4389@snu.ac.kr>
2023-11-14 15:41:28 +0900

    :sparkles: mock repo

--- my_stock/lib/di.dart
+++ my_stock/lib/di.dart
@@ -1,15 +1,15 @@
 import 'package:get_it/get_it.dart';
-import 'package:my_stock/app/data/repository_impl/emotion_repository_impl.dart';
+import 'package:my_stock/app/data/mock_impl/mock_auth_service_impl.dart';
+import 'package:my_stock/app/data/mock_impl/mock_emotion_repository_impl.dart';
+import 'package:my_stock/app/data/mock_impl/mock_stock_repository_impl.dart';
+import 'package:my_stock/app/data/mock_impl/mock_user_repository_impl.dart';
 import 'package:my_stock/app/data/repository_impl/local_repository_impl.dart';
-import 'package:my_stock/app/data/repository_impl/stock_repository_impl.dart';
-import 'package:my_stock/app/data/repository_impl/user_repository_impl.dart';
 import 'package:my_stock/app/domain/repository_interface/emotion_repostory.dart';
 import 'package:my_stock/app/domain/repository_interface/local_repository.dart';
 import 'package:my_stock/app/domain/repository_interface/stock_repository.dart';
 import 'package:my_stock/app/domain/repository_interface/user_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'app/data/service_impl/auth_service_impl.dart';
 import 'app/domain/service_interface/auth_service.dart';
 
 GetIt getIt = GetIt.instance;
@@ -17,9 +17,9 @@ GetIt getIt = GetIt.instance;
 Future<void> setup() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   getIt.registerSingleton<SharedPreferences>(prefs);
-  getIt.registerSingleton<AuthService>(AuthServiceImpl());
+  getIt.registerSingleton<AuthService>(MockAuthServiceImpl());
   getIt.registerSingleton<LocalRepository>(LocalRepositoryImpl());
-  getIt.registerSingleton<StockRepository>(StockRepositoryImpl());
-  getIt.registerSingleton<UserRepository>(UserRepositoryImpl());
-  getIt.registerSingleton<EmotionRepository>(EmotionRepositoryImpl());
+  getIt.registerSingleton<StockRepository>(MockStockRepositoryImpl());
+  getIt.registerSingleton<UserRepository>(MockUserRepositoryImpl());
+  getIt.registerSingleton<EmotionRepository>(MockEmotionRepositoryImpl());
 }
