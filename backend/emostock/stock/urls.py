80c500a841ea2f7201734c82be502ae3029380c8
Smile-Ky <keunyoung98@snu.ac.kr>
2023-11-20 12:26:16 +0900

    :sparkles: Add url for UserBalance

--- backend/emostock/stock/urls.py
+++ backend/emostock/stock/urls.py
@@ -1,7 +1,8 @@
 from emostock.common.urls import viewset_path
-from stock.views import StockView, MyStockView
+from stock.views import StockView, MyStockView, UserBalanceView
 
 urlpatterns = [
     viewset_path(StockView, "stock"),
     viewset_path(MyStockView, "mystock"),
+    viewset_path(UserBalanceView, "balance"),
 ]
