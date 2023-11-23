6409954cb35340835347991d8f35539bd0f4c6a1
junseok Lee <123jellybean@snu.ac.kr>
2023-11-23 16:55:15 +0900

    :sparkles: 주식 기록 6글자 제한

--- my_stock/lib/app/presentation/screen/stock_register_screen/add_history_screen/add_history_screen_view_model.dart
+++ my_stock/lib/app/presentation/screen/stock_register_screen/add_history_screen/add_history_screen_view_model.dart
@@ -23,11 +23,21 @@ class AddHistoryScreenViewModel with ChangeNotifier {
       notifyListeners();
     });
     priceController.addListener(() {
+      String text = priceController.text.replaceAll(",", "");
+      if (text.length > 6) {
+        text = text.substring(0, 6);
+        priceController.text = _formatter.format(int.parse(text));
+      }
       priceController.selection =
           TextSelection.fromPosition(TextPosition(offset: priceController.text.length));
       notifyListeners();
     });
     quantityController.addListener(() {
+      String text = quantityController.text.replaceAll(",", "");
+      if (text.length > 6) {
+        text = text.substring(0, 6);
+        quantityController.text = _formatter.format(int.parse(text));
+      }
       quantityController.selection =
           TextSelection.fromPosition(TextPosition(offset: quantityController.text.length));
       notifyListeners();
