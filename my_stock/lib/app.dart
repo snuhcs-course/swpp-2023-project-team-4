7de2fe12faad9fe4d3692d0b389343f6e88186b9
LimSusu <dlatn4389@snu.ac.kr>
2023-11-23 21:22:30 +0900

    :sparkles: 시스템 하단 바 색상 설정

--- my_stock/lib/app.dart
+++ my_stock/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:my_stock/app/presentation/screen/bottom_nav_0_main_indexed_stack_screen/main_indexed_stack_screen.dart';
 import 'package:my_stock/app/presentation/screen/entry_flow/nickname_screen/nickname_screen.dart';
@@ -6,12 +7,16 @@ import 'package:my_stock/app/presentation/screen/entry_flow/sign_in_screen/sign_
 import 'package:my_stock/app/presentation/screen/search_stock_screen/search_stock_screen.dart';
 import 'package:my_stock/app/presentation/screen/splash_screen/splash_screen.dart';
 import 'package:my_stock/app/presentation/util/navigator_key.dart';
+import 'package:my_stock/core/theme/color_theme.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: EmotionColor.notFilled,
+    ));
     return MaterialApp(
       navigatorKey: NavigatorKey.key,
       theme: ThemeData(
