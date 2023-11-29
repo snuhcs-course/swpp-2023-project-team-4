7f7ba2bd3971410dbf81a7f699cb179ed2c65dd3
Smile-Ky <keunyoung98@snu.ac.kr>
2023-11-29 19:43:15 +0900

    Update Report serializer

--- backend/emostock/report/serializers.py
+++ backend/emostock/report/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
-from report.models import Report
 
 
 class ReportSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Report
         fields = [
-            "date",
-            "summary",
-            "stock",
+            "title",
+            "body",
         ]
