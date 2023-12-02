73fb7e5c69fc376b9de049c8c2fe083cfaad0644
LimSusu <dlatn4389@snu.ac.kr>
2023-12-02 17:56:59 +0900

    :sparkles: 주식 분석 보여주기 형태만

--- my_stock/lib/app/presentation/screen/bottom_nav_3_report_screen/report_screen.dart
+++ my_stock/lib/app/presentation/screen/bottom_nav_3_report_screen/report_screen.dart
@@ -1,5 +1,7 @@
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:my_stock/app/domain/model/user.dart';
 import 'package:my_stock/app/presentation/screen/bottom_nav_2_calendar_screen/date_emotion.dart';
 import 'package:my_stock/app/presentation/screen/bottom_nav_3_report_screen/report_screen_view_model.dart';
 import 'package:my_stock/app/presentation/util/my_navigator.dart';
@@ -13,6 +15,7 @@ import 'package:provider/provider.dart';
 part 'widget/calendar.dart';
 part 'widget/circle.dart';
 part 'widget/column_element.dart';
+part 'widget/emotion_example.dart';
 
 class ReportScreen extends StatelessWidget {
   const ReportScreen({super.key});
@@ -24,114 +27,169 @@ class ReportScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: BackgroundColor.defaultColor,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              children: [
-                const SizedBox(height: 30),
-                Builder(builder: (context) {
-                  void Function(Date, Date) setDate = context.read<ReportScreenViewModel>().setDate;
-                  return GestureDetector(
-                    onTap: () {
-                      showDialog(context: context, builder: (_) => Dialog(child: _CalendarDialog()))
-                          .then((value) {
-                        if (value == null) return;
-                        setDate(value.$1, value.$2);
-                      });
-                    },
-                    behavior: HitTestBehavior.opaque,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Consumer<ReportScreenViewModel>(
-                            builder: (_, viewModel, __) {
-                              (int, int, int) selectedNthWeek = viewModel.selectedNthWeek;
-                              int year = selectedNthWeek.$1;
-                              int month = selectedNthWeek.$2;
-                              int week = selectedNthWeek.$3;
-                              return Text(
-                                "$year년 $month월 $week주차",
-                                style: HeaderTextStyle.nanum16.writeText,
-                              );
-                            },
-                          ),
-                          const SizedBox(width: 10),
-                          Image.asset("assets/images/downward_arrow.png"),
-                        ],
-                      ),
-                    ),
-                  );
-                }),
-                const SizedBox(height: 20),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 29.5).copyWith(
-                    top: 15,
-                    bottom: 20,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    children: [
-                      Text("기분 흐름 그래프", style: BodyTextStyle.nanum12),
-                      const SizedBox(height: 9),
-                      Container(width: double.infinity, height: 1, color: Colors.black),
-                      const SizedBox(height: 11),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(width: 4),
-                          Column(
-                            children: [
-                              _Circle(EmotionColor.happier),
-                              const SizedBox(height: 10),
-                              _Circle(EmotionColor.happy),
-                              const SizedBox(height: 10),
-                              _Circle(EmotionColor.neutral),
-                              const SizedBox(height: 10),
-                              _Circle(EmotionColor.sad),
-                              const SizedBox(height: 10),
-                              _Circle(EmotionColor.sadder),
-                            ],
-                          ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: LayoutBuilder(
-                              builder: (context, constraints) {
-                                double maxWidth = constraints.maxWidth;
-                                double width = (maxWidth - 1) / 5;
-                                return Consumer<ReportScreenViewModel>(builder: (_, viewModel, __) {
-                                  return Stack(
-                                    children: [
-                                      Row(
-                                        children: viewModel.dates.map((date) {
-                                          return _ColumnElement(
-                                            date: date,
-                                            width: width,
-                                          );
-                                        }).toList(),
-                                      ),
-                                      _graphLine(maxWidth, viewModel.dateEmotions),
-                                      ..._graphCircles(viewModel.dateEmotions, width),
-                                    ],
-                                  );
-                                });
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  Builder(builder: (context) {
+                    void Function(Date, Date) setDate =
+                        context.read<ReportScreenViewModel>().setDate;
+                    return GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (_) => Dialog(child: _CalendarDialog())).then((value) {
+                          if (value == null) return;
+                          setDate(value.$1, value.$2);
+                        });
+                      },
+                      behavior: HitTestBehavior.opaque,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Consumer<ReportScreenViewModel>(
+                              builder: (_, viewModel, __) {
+                                (int, int, int) selectedNthWeek = viewModel.selectedNthWeek;
+                                int year = selectedNthWeek.$1;
+                                int month = selectedNthWeek.$2;
+                                int week = selectedNthWeek.$3;
+                                return Text(
+                                  "$year년 $month월 $week주차",
+                                  style: HeaderTextStyle.nanum16.writeText,
+                                );
                               },
                             ),
-                          ),
-                          const SizedBox(width: 3),
-                        ],
+                            const SizedBox(width: 10),
+                            Image.asset("assets/images/downward_arrow.png"),
+                          ],
+                        ),
                       ),
-                      const SizedBox(height: 9),
-                      Container(width: double.infinity, height: 1, color: Colors.black),
-                    ],
+                    );
+                  }),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 29.5).copyWith(
+                      top: 15,
+                      bottom: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Text("기분 흐름 그래프", style: BodyTextStyle.nanum12),
+                        const SizedBox(height: 9),
+                        Container(width: double.infinity, height: 1, color: Colors.black),
+                        const SizedBox(height: 11),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(width: 4),
+                            Column(
+                              children: [
+                                _Circle(EmotionColor.happier),
+                                const SizedBox(height: 10),
+                                _Circle(EmotionColor.happy),
+                                const SizedBox(height: 10),
+                                _Circle(EmotionColor.neutral),
+                                const SizedBox(height: 10),
+                                _Circle(EmotionColor.sad),
+                                const SizedBox(height: 10),
+                                _Circle(EmotionColor.sadder),
+                              ],
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: LayoutBuilder(
+                                builder: (context, constraints) {
+                                  double maxWidth = constraints.maxWidth;
+                                  double width = (maxWidth - 1) / 5;
+                                  return Consumer<ReportScreenViewModel>(
+                                      builder: (_, viewModel, __) {
+                                    return Stack(
+                                      children: [
+                                        Row(
+                                          children: viewModel.dates.map((date) {
+                                            return _ColumnElement(
+                                              date: date,
+                                              width: width,
+                                            );
+                                          }).toList(),
+                                        ),
+                                        _graphLine(maxWidth, viewModel.dateEmotions),
+                                        ..._graphCircles(viewModel.dateEmotions, width),
+                                      ],
+                                    );
+                                  });
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 3),
+                          ],
+                        ),
+                        const SizedBox(height: 9),
+                        Container(width: double.infinity, height: 1, color: Colors.black),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30).copyWith(
+                      top: 15,
+                      bottom: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text("Emostock만의 주식 분석", style: BodyTextStyle.nanum12.black),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(width: double.infinity, height: 1, color: Colors.black),
+                        const SizedBox(height: 20),
+                        Builder(builder: (context) {
+                          List<EmotionVMEnum> emotions = [
+                            EmotionVMEnum.happier,
+                            EmotionVMEnum.happy,
+                            EmotionVMEnum.neutral,
+                            EmotionVMEnum.sad,
+                            EmotionVMEnum.sadder,
+                          ];
+                          List<Widget> widgets = [];
+                          for (int i = 0; i < emotions.length; i++) {
+                            widgets.add(
+                              Expanded(
+                                child: _EmotionExample(
+                                  index: i + 1,
+                                  emotion: emotions[i],
+                                ),
+                              ),
+                            );
+                          }
+                          return Row(
+                            children: widgets,
+                          );
+                        }),
+                        const SizedBox(height: 30),
+                        Text("${GetIt.I<User>().nickname}님을 위한 주간 분석",
+                            style: HeaderTextStyle.nanum16),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
