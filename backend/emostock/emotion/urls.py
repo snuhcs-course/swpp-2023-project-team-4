052ea2af4446de19b4cf5fd9a08695bc172874ed
Junseok Lee <123jellybean@snu.ac.kr>
2023-12-03 13:00:00 +0900

    Implement EmotionListByRangeView
    List emotions by start date and end date

--- backend/emostock/emotion/urls.py
+++ backend/emostock/emotion/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('<int:year>/<int:month>/', views.EmotionListView.as_view()),
     path('<int:year>/<int:month>/<int:day>/', views.EmotionUpdateView.as_view()),
+    path('', views.EmotionListByRangeView.as_view())
 ]
